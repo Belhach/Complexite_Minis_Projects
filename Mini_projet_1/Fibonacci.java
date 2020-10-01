eac060026f4ff22b8fec1cbb8d3d1b3c51c1414b
mhamedbelhachemi <mhamed79@live.fr>
2020-10-01 09:20:22 +0200

    1- implementation of fibonacci iterative method 2- implementation of fibonacci recursive method

--- Mini_projet_1/Fibonacci.java
+++ Mini_projet_1/Fibonacci.java
@@ -13,6 +13,19 @@
 **************************************************************************************************************************************************************/
 
 class Fibonacci{
+
+    public int fibonacciIterative(int n){
+        if(n <= 1) return n;
+        int fib = 1;
+        return fib;
+    }
+
+    public int fibonacciRecursive(int n){
+        return 0;
+    }
+}
+
+class Main{
     public static void main(String[] args) {
         System.out.println("fibo fibonacci babe");
     }
