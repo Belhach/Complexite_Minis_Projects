b8a864f98954a413786a6ed9e2dfb3d367a6526b
mhamedbelhachemi <mhamed79@live.fr>
2020-10-30 22:36:01 +0100

    "finition Fibonacci"

--- src/Mini_projet_1/Fibonacci.java
+++ src/Mini_projet_1/Fibonacci.java
@@ -14,7 +14,7 @@ package Mini_projet_1;
 *                                                                                                                                                                  *
 *******************************************************************************************************************************************************************/
 
-class Fibonacci{
+public class Fibonacci{
 
     //iterative version of fibonacci
     public int fibonacciIterative(int n){
@@ -36,21 +36,33 @@ class Fibonacci{
         return fibonacciRecursive(n-1) + fibonacciRecursive(n-2);
     }
 
-    public void fibonacci(int n, String version){
-        if(version == "iterative"){
+    public void fibonacci(int n){
+              
+            // Calcule de temps pour la version iterative
+            long tempsDebut = System.nanoTime();
             System.out.println("la version iterative pour la valeur "+n+" retourne le resultat : "+fibonacciIterative(n));
-        }
-        else if(version == "recursive"){
-            System.out.println("la version iterative pour la valeur "+n+" retourne le resultat : "+fibonacciRecursive(n));
-        }           
-    }
-}
+            long tempsFin = System.nanoTime();
+            float seconds = (tempsFin - tempsDebut) / 1000F;
+            System.out.println("Temps d'execution pout la version iteeative: "+ Float.toString(seconds) + " nano secondes.");
 
-class Main{
+            // Calcule de temps pour la version recursive
+            long tempsDebut1 = System.nanoTime();
+            System.out.println("la version iterative pour la valeur "+n+" retourne le resultat : "+fibonacciRecursive(n));
+            long tempsFin1 = System.nanoTime();
+            float seconds1 = (tempsFin1 - tempsDebut1) / 1000F;
+            System.out.println("Temps d'execution pout la version recursive: "+ Float.toString(seconds1) + " nano secondes.");
+    }
+
     public static void main(String[] args) {
 
         Fibonacci fibonacci = new Fibonacci();
-        fibonacci.fibonacci(10,"iterative");
+        if(args.length == 1){
+            fibonacci.fibonacci(Integer.parseInt(args[0]));
+        }
+        else{
+            System.out.println("Pour lancer le programme il faut fournir un argument \n arg[0] = nombre qu'on veut calculer");
+            System.out.println("l'execution doit etre sous la forme \"java Fibonacci.java arg[0]\"");
+        }
 
     }
 }
